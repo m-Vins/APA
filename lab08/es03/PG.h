69e18b5b9636acd2ab172797c7d703ca513713d9
Vincenzo <58368395+m-Vins@users.noreply.github.com>
2019-12-13 13:00:45 +0100

    Create PG.h

---
+++ lab08/es03/PG.h
@@ -0,0 +1,28 @@
+//
+// Created by Master on 12/12/2019.
+//
+
+#ifndef ES_03_LAB08_PG_H
+#define ES_03_LAB08_PG_H
+#define MAX 50+1
+
+#include <stdio.h>
+#include "INV.h"
+
+typedef struct node *LINK;
+typedef struct tabPg_t *TABPG;
+
+
+TABPG creaListaPg(void);
+void loadPg(TABPG tabPg, FILE *fp);
+LINK searchPg(TABPG tabPg,char *key);
+void deletePg(TABPG tabPg, char *key);
+void editEquipPg(TABPG tabPg,char *key,TABINV tabInv);
+void displayStat(TABPG tabPg,char *key);
+void displayPg(TABPG tabPg, char *key);
+void freeTabList(TABPG tabPg);
+void displayList(TABPG tabPg);
+
+
+
+#endif //ES_03_LAB08_PG_H
